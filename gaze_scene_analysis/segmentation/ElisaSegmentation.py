b192c3718e5c17cb979a8da201724b34dcaa22eb
Elisa <elisa2b@hotmail.com>
2026-01-08 19:49:47 +0100

    liste objets

--- gaze_scene_analysis/segmentation/ElisaSegmentation.py
+++ gaze_scene_analysis/segmentation/ElisaSegmentation.py
@@ -1,15 +1,15 @@
 from gaze_scene_analysis.segmentation import SegmentationInterface
 from gaze_scene_analysis.types import FrameData, LookedObject
 
-OBJECTS : list[str] = [
-    "humain",
-    "voiture",
-    "vélo",
-    "moto",
-    "panneau",
-    "feu de signalisation",
-    "batiment",
-    "route"
+OBJECTS : list[str] = [ # en français, et en commentaire le nom du label dans le Cityscapes dataset
+    "humain", # person + rider
+    "véhicule motorisé", # car + truck + bus + motorcycle
+    "vélo", # bicycle
+    "panneau", # traffic sign
+    "feu de signalisation", # traffic light
+    "poteau", # pole
+    "batiment", # building
+    "route" # road
 ]
 
 class ElisaSegmentation(SegmentationInterface):
